e3bda0de76fc392ce6b6010915c13144da6c7524
Nana Kofi Larbi Mantey <nanakofilarbimantey@gmail.com>
2024-10-28 17:02:10 +0000

    updates replace variables deployment script

--- deployment/replace-variables.sh
+++ deployment/replace-variables.sh
@@ -7,10 +7,16 @@ if [ -z "${API_URI}" ]; then
     exit 1
 fi
 
-# Find and replace baked values with real values for the API_URI
+if [ -z "${AWS_REGION}" ]; then
+    echo "AWS_REGION is not set. Exiting..."
+    exit 1
+fi
+
+# Find and replace baked values with real values for the API_URI AND AWS_REGION
 find /app/packages/dashboard/public /app/packages/dashboard/.next -type f -name "*.js" |
 while read file; do
     sed -i "s|PLUNK_API_URI|${API_URI}|g" "$file"
+    sed -i "s|PLUNK_AWS_REGION|${AWS_REGION}|g" "$file"
 done
 
 echo "Environment Variables Baked."
