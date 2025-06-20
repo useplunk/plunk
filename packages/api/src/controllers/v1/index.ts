15a3b05d8b6e7515b7948133d82380e9f0a04f09
itsrofly <99050170+itsrofly@users.noreply.github.com>
2025-06-20 23:54:24 +0100

    brute migration to zod v4

--- packages/api/src/controllers/v1/index.ts
+++ packages/api/src/controllers/v1/index.ts
@@ -1,11 +1,21 @@
-import { ChildControllers, Controller, Middleware, Post } from "@overnightjs/core";
+import {
+  ChildControllers,
+  Controller,
+  Middleware,
+  Post,
+} from "@overnightjs/core";
 import { EventSchemas } from "@plunk/shared";
 import dayjs from "dayjs";
 import type { Request, Response } from "express";
 import signale from "signale";
 import { prisma } from "../../database/prisma";
 import { HttpException, NotAllowed } from "../../exceptions";
-import { type IKey, type ISecret, isValidKey, isValidSecretKey } from "../../middleware/auth";
+import {
+  type IKey,
+  type ISecret,
+  isValidKey,
+  isValidSecretKey,
+} from "../../middleware/auth";
 import { ActionService } from "../../services/ActionService";
 import { ContactService } from "../../services/ContactService";
 import { EmailService } from "../../services/EmailService";
@@ -20,235 +30,270 @@ import { Events } from "./Events";
 import { Templates } from "./Templates";
 
 @Controller("v1")
-@ChildControllers([new Actions(), new Templates(), new Campaigns(), new Contacts(), new Events()])
+@ChildControllers([
+  new Actions(),
+  new Templates(),
+  new Campaigns(),
+  new Contacts(),
+  new Events(),
+])
 export class V1 {
-	@Post()
-	@Post("track")
-	@Middleware([isValidKey])
-	public async postEvent(req: Request, res: Response) {
-		const { key } = res.locals.auth as IKey;
+  @Post()
+  @Post("track")
+  @Middleware([isValidKey])
+  public async postEvent(req: Request, res: Response) {
+    const { key } = res.locals.auth as IKey;
 
-		const project = await ProjectService.key(key);
+    const project = await ProjectService.key(key);
 
-		if (!project) {
-			throw new HttpException(401, "Incorrect Bearer token specified");
-		}
+    if (!project) {
+      throw new HttpException(401, "Incorrect Bearer token specified");
+    }
 
-		const result = EventSchemas.post.safeParse(req.body);
+    const result = EventSchemas.post.safeParse(req.body);
 
-		if (!result.success) {
-			signale.warn(`${project.name} tried tracking an event with invalid data: ${JSON.stringify(req.body)}`);
-			if ("unionErrors" in result.error.issues[0]) {
+    if (!result.success) {
+      signale.warn(
+        `${project.name} tried tracking an event with invalid data: ${JSON.stringify(req.body)}`,
+      );
+      // No unionErrors reference
+      /* if ("unionErrors" in result.error.issues[0]) {
 				throw new HttpException(400, result.error.issues[0].unionErrors[0].errors[0].message);
-			}
+				} */
 
-			throw new HttpException(400, result.error.issues[0].message);
-		}
+      throw new HttpException(400, result.error.issues[0].message);
+    }
 
-		const { event: name, email, data, subscribed } = result.data;
+    const { event: name, email, data, subscribed } = result.data;
 
-		if (name === "subscribe" || name === "unsubscribe") {
-			throw new NotAllowed("subscribe & unsubscribe are reserved event names.");
-		}
+    if (name === "subscribe" || name === "unsubscribe") {
+      throw new NotAllowed("subscribe & unsubscribe are reserved event names.");
+    }
 
-		let event = await EventService.event(project.id, name);
+    let event = await EventService.event(project.id, name);
 
-		if (!event) {
-			event = await prisma.event.create({
-				data: { name, projectId: project.id },
-			});
-			redis.set(Keys.Event.event(project.id, event.name), JSON.stringify(event));
-			redis.set(Keys.Event.id(event.id), JSON.stringify(event));
+    if (!event) {
+      event = await prisma.event.create({
+        data: { name, projectId: project.id },
+      });
+      redis.set(
+        Keys.Event.event(project.id, event.name),
+        JSON.stringify(event),
+      );
+      redis.set(Keys.Event.id(event.id), JSON.stringify(event));
 
-			redis.del(Keys.Project.events(project.id, true));
-			redis.del(Keys.Project.events(project.id, false));
-		}
+      redis.del(Keys.Project.events(project.id, true));
+      redis.del(Keys.Project.events(project.id, false));
+    }
 
-		let contact = await ContactService.email(project.id, email);
+    let contact = await ContactService.email(project.id, email);
 
-		if (!contact) {
-			contact = await prisma.contact.create({
-				data: {
-					email,
-					subscribed: subscribed ?? true,
-					projectId: project.id,
-				},
-			});
+    if (!contact) {
+      contact = await prisma.contact.create({
+        data: {
+          email,
+          subscribed: subscribed ?? true,
+          projectId: project.id,
+        },
+      });
 
-			redis.del(Keys.Contact.id(contact.id));
-			redis.del(Keys.Contact.email(project.id, contact.email));
-		} else {
-			if (subscribed !== null && contact.subscribed !== subscribed) {
-				contact = await prisma.contact.update({where: {id: contact.id}, data: {subscribed}});
-		
-				redis.del(Keys.Contact.id(contact.id));
-				redis.del(Keys.Contact.email(project.id, contact.email));
-			  }
-		}
+      redis.del(Keys.Contact.id(contact.id));
+      redis.del(Keys.Contact.email(project.id, contact.email));
+    } else {
+      if (subscribed !== null && contact.subscribed !== subscribed) {
+        contact = await prisma.contact.update({
+          where: { id: contact.id },
+          data: { subscribed },
+        });
 
-		if (data) {
-			const givenUserData = Object.entries(data);
-			const userData = JSON.parse(contact.data ?? "{}");
-			const dataToUpdate = JSON.parse(contact.data ?? "{}");
+        redis.del(Keys.Contact.id(contact.id));
+        redis.del(Keys.Contact.email(project.id, contact.email));
+      }
+    }
 
-			givenUserData.forEach(([key, value]) => {
-				userData[key] = value.value;
-				if (value.persistent) {
-					dataToUpdate[key] = value.value;
-				}
-			});
+    if (data) {
+      const givenUserData = Object.entries(data);
+      const userData = JSON.parse(contact.data ?? "{}");
+      const dataToUpdate = JSON.parse(contact.data ?? "{}");
 
-			contact.data = JSON.stringify(userData);
+      givenUserData.forEach(([key, value]) => {
+        userData[key] = value.value;
+        if (value.persistent) {
+          dataToUpdate[key] = value.value;
+        }
+      });
 
-			await prisma.contact.update({
-				where: { id: contact.id },
-				data: { data: JSON.stringify(dataToUpdate) },
-			});
-		}
+      contact.data = JSON.stringify(userData);
 
-		await prisma.trigger.create({
-			data: { eventId: event.id, contactId: contact.id },
-		});
+      await prisma.contact.update({
+        where: { id: contact.id },
+        data: { data: JSON.stringify(dataToUpdate) },
+      });
+    }
 
-		void ActionService.trigger({ event, contact, project });
+    await prisma.trigger.create({
+      data: { eventId: event.id, contactId: contact.id },
+    });
 
-		signale.success(`${project.name} triggered ${event.name} for ${contact.email}`);
+    void ActionService.trigger({ event, contact, project });
 
-		return res.status(200).json({
-			success: true,
-			contact: contact.id,
-			event: event.id,
-			timestamp: dayjs().toISOString(),
-		});
-	}
+    signale.success(
+      `${project.name} triggered ${event.name} for ${contact.email}`,
+    );
 
-	@Post("send")
-	@Middleware([isValidSecretKey])
-	public async send(req: Request, res: Response) {
-		const { sk } = res.locals.auth as ISecret;
+    return res.status(200).json({
+      success: true,
+      contact: contact.id,
+      event: event.id,
+      timestamp: dayjs().toISOString(),
+    });
+  }
 
-		const project = await ProjectService.secret(sk);
+  @Post("send")
+  @Middleware([isValidSecretKey])
+  public async send(req: Request, res: Response) {
+    const { sk } = res.locals.auth as ISecret;
 
-		if (!project) {
-			throw new HttpException(401, "Incorrect Bearer token specified");
-		}
+    const project = await ProjectService.secret(sk);
 
-		const result = EventSchemas.send.safeParse(req.body);
+    if (!project) {
+      throw new HttpException(401, "Incorrect Bearer token specified");
+    }
 
-		if (!result.success) {
-			if ("unionErrors" in result.error.issues[0]) {
-				throw new HttpException(400, result.error.issues[0].unionErrors[0].errors[0].message);
-			}
+    const result = EventSchemas.send.safeParse(req.body);
 
-			throw new HttpException(400, result.error.issues[0].message);
-		}
+    if (!result.success) {
+      // No unionErrors reference
+      /*
+      if ("unionErrors" in result.error.issues[0]) {
+        throw new HttpException(
+          400,
+          result.error.issues[0].unionErrors[0].errors[0].message,
+        );
+      }
+      */
 
-		const { from, name, reply, to, subject, body, subscribed, headers } = result.data;
+      throw new HttpException(400, result.error.issues[0].message);
+    }
 
-		if (!project.email || !project.verified) {
-			throw new HttpException(401, "Verify your domain before you start sending");
-		}
+    const { from, name, reply, to, subject, body, subscribed, headers } =
+      result.data;
 
-		if (from && from.split("@")[1] !== project.email?.split("@")[1]) {
-			throw new HttpException(401, "Custom from address must be from a verified domain");
-		}
+    if (!project.email || !project.verified) {
+      throw new HttpException(
+        401,
+        "Verify your domain before you start sending",
+      );
+    }
 
-		const emails: {
-			contact: {
-				id: string;
-				email: string;
-			};
-			email: string;
-		}[] = [];
+    if (from && from.split("@")[1] !== project.email?.split("@")[1]) {
+      throw new HttpException(
+        401,
+        "Custom from address must be from a verified domain",
+      );
+    }
 
-		for (const email of to) {
-			let contact = await ContactService.email(project.id, email);
+    const emails: {
+      contact: {
+        id: string;
+        email: string;
+      };
+      email: string;
+    }[] = [];
 
-			if (!contact) {
-				contact = await prisma.contact.create({
-					data: {
-						email,
-						subscribed: subscribed ?? false,
-						projectId: project.id,
-					},
-				});
+    for (const email of to) {
+      let contact = await ContactService.email(project.id, email);
 
-				redis.del(Keys.Contact.id(contact.id));
-				redis.del(Keys.Contact.email(project.id, contact.email));
-			} else {
-				if (subscribed && contact.subscribed !== subscribed) {
-					await prisma.contact.update({
-						where: { id: contact.id },
-						data: { subscribed },
-					});
-					redis.set(
-						Keys.Contact.email(project.id, contact.email),
-						JSON.stringify({
-							...contact,
-							subscribed,
-						}),
-					);
-					redis.del(Keys.Contact.id(contact.id));
-				}
-			}
+      if (!contact) {
+        contact = await prisma.contact.create({
+          data: {
+            email,
+            subscribed: subscribed ?? false,
+            projectId: project.id,
+          },
+        });
 
-			const { subject: enrichedSubject, body: enrichedBody } = EmailService.format({
-				subject,
-				body,
-				data: {
-					plunk_id: contact.id,
-					plunk_email: contact.email,
-					...JSON.parse(contact.data ?? "{}"),
-				},
-			});
+        redis.del(Keys.Contact.id(contact.id));
+        redis.del(Keys.Contact.email(project.id, contact.email));
+      } else {
+        if (subscribed && contact.subscribed !== subscribed) {
+          await prisma.contact.update({
+            where: { id: contact.id },
+            data: { subscribed },
+          });
+          redis.set(
+            Keys.Contact.email(project.id, contact.email),
+            JSON.stringify({
+              ...contact,
+              subscribed,
+            }),
+          );
+          redis.del(Keys.Contact.id(contact.id));
+        }
+      }
 
-			const { messageId } = await EmailService.send({
-				from: {
-					name: name ?? project.from ?? project.name,
-					email: from ?? project.email,
-				},
-				reply: reply ?? from ?? project.email,
-				to: [email],
-				headers,
-				content: {
-					subject: enrichedSubject,
-					html: EmailService.compile({
-						isHtml: true,
-						content: enrichedBody,
-						footer: {
-							unsubscribe: false,
-						},
-						contact: {
-							id: contact.id,
-						},
-						project: {
-							name: project.name,
-						},
-					}),
-				},
-			});
+      const { subject: enrichedSubject, body: enrichedBody } =
+        EmailService.format({
+          subject,
+          body,
+          data: {
+            plunk_id: contact.id,
+            plunk_email: contact.email,
+            ...JSON.parse(contact.data ?? "{}"),
+          },
+        });
 
-			const createdEmail = await prisma.email.create({
-				data: {
-					messageId,
-					subject,
-					body: enrichedBody,
-					contactId: contact.id,
-					projectId: project.id,
-				},
-			});
+      const { messageId } = await EmailService.send({
+        from: {
+          name: name ?? project.from ?? project.name,
+          email: from ?? project.email,
+        },
+        reply: reply ?? from ?? project.email,
+        to: [email],
+        headers,
+        content: {
+          subject: enrichedSubject,
+          html: EmailService.compile({
+            isHtml: true,
+            content: enrichedBody,
+            footer: {
+              unsubscribe: false,
+            },
+            contact: {
+              id: contact.id,
+            },
+            project: {
+              name: project.name,
+            },
+          }),
+        },
+      });
 
-			emails.push({
-				contact: { id: contact.id, email: contact.email },
-				email: createdEmail.id,
-			});
-		}
+      const createdEmail = await prisma.email.create({
+        data: {
+          messageId,
+          subject,
+          body: enrichedBody,
+          contactId: contact.id,
+          projectId: project.id,
+        },
+      });
 
-		redis.del(Keys.Project.emails(project.id));
-		redis.del(Keys.Project.emails(project.id, { count: true }));
+      emails.push({
+        contact: { id: contact.id, email: contact.email },
+        email: createdEmail.id,
+      });
+    }
 
-		signale.success(`${project.name} sent a transactional email to ${to.join(", ")}`);
+    redis.del(Keys.Project.emails(project.id));
+    redis.del(Keys.Project.emails(project.id, { count: true }));
 
-		return res.status(200).json({ success: true, emails, timestamp: dayjs().toISOString() });
-	}
+    signale.success(
+      `${project.name} sent a transactional email to ${to.join(", ")}`,
+    );
+
+    return res
+      .status(200)
+      .json({ success: true, emails, timestamp: dayjs().toISOString() });
+  }
 }
